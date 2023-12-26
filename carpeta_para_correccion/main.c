cb27862b97085d1eb1e06769c65744cb4ee06260
Román Ignacio Muñoz <rommunoz@frba.utn.edu.ar>
2023-12-26 15:22:47 -0300

    Algoritmo mas corto y mejorado
    Y lo pongo en esta carpeta para no marear, ahora mismo estoy de vacaciones y no tengo mi computadora de escritorio para acomodarlo con git desde consola, etc.

---
+++ carpeta_para_correccion/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "scanner.h"
+
+int main(void){
+    while(token != FDA){
+        token = scanner();
+        switch(token){
+            case ERROR_GENERAL:
+                printf("Error general '%s'\n", lexem_buffer);
+                break;
+            case IDENTIFICADOR:
+                printf("Identificador '%s'\n", lexem_buffer);
+                break;
+            case (ENTERO || ENTERO_CERO):
+                printf("Entero '%s'\n", lexem_buffer);
+                break;
+            case HEXADECIMAL: 
+                printf("Hexadecimal '%s'\n", lexem_buffer); 
+                break;
+            case ENT_MAL_FORM: 
+                printf("Entero Mal formado '%s'\n", lexem_buffer);  
+                break;
+            case FDA:
+                printf("Se llego al fin de archivo.\n");  
+                break;
+        }
+    }
+    return 0;
+}
